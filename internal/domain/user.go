fce9ba38882b00dc3533387ce2566212f0e09b58
woods-one <0820ricer@gmail.com>
2026-02-18 23:34:31 +0900

    add: userのdomainを追加

---
+++ internal/domain/user.go
@@ -0,0 +1,24 @@
+package domain
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID             string    `json:"userID"`
+	UserName       string    `json:"userName"`
+	Icon           string    `json:"icon,omitempty"`// アイコン、stringにしてるが実際どうなるかはわからない
+	ProfileMessage string    `json:"profileMsg,omitempty"`
+
+	Birthday      time.Time `json:"birthday"`
+	RegisteredAt  time.Time `json:"registerdDate"`
+	LatestLoginAt time.Time `json:"latestLoginDate"`
+	StreakLogin   int       `json:"streakLogin"`
+}
+
+func NewUserID() UserID {
+	return UserID(uuid.NewString())
+}
+
+
