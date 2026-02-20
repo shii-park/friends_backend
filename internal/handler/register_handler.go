f49536cd713ccdc5cb15233281f4ddd18eb14e61
Suiren91 <rento8191@gmail.com>
2026-02-21 01:11:21 +0900

    #20 Fix: jsonの構造体の修正とdomain.NewUser()の引数の修正

--- internal/handler/register_handler.go
+++ internal/handler/register_handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/shii-park/friends/internal/domain"
@@ -11,14 +12,17 @@ import (
 // RegisterHandler はユーザー新規登録を処理するハンドラーです。
 func RegisterHandler(c *gin.Context) {
 	var json struct {
-		Username string `json:"Username" binding:"required"`
+		Username       string    `json:"Username" binding:"required"`
+		Icon           string    `json:"icon.omitempty"`
+		ProfileMessage string    `json:"profileMsg.omitempty"`
+		Birthday       time.Time `json:"birthday"`
 	}
 
 	if err := c.ShouldBindBodyWithJSON(&json); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	user, err := domain.NewUser(json.Username, nil, nil, nil)
+	user, err := domain.NewUser(json.Username, json.Icon, json.ProfileMessage, json.Birthday)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
