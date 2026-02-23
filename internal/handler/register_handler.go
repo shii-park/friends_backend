b76b1a23424476ccad8bb13f1e857634d656a1e7
Suiren91 <rento8191@gmail.com>
2026-02-23 18:56:02 +0900

    #20 Fix: Birthdayのrequired指定と未来の日付のバリデーション

--- internal/handler/register_handler.go
+++ internal/handler/register_handler.go
@@ -15,13 +15,21 @@ func RegisterHandler(c *gin.Context) {
 		Username       string    `json:"userName" binding:"required"`
 		Icon           string    `json:"icon,omitempty"`
 		ProfileMessage string    `json:"profileMsg,omitempty"`
-		Birthday       time.Time `json:"birthday"`
+		Birthday       time.Time `json:"birthday" binding:"required"`
 	}
 
 	if err := c.ShouldBindBodyWithJSON(&json); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	//日付の検証(未来の日付であればここでエラー)
+	now := time.Now()
+	if json.Birthday.After(now) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "誕生日が正しくありません"})
+		return
+	}
+
 	user, err := domain.NewUser(json.Username, json.Icon, json.ProfileMessage, json.Birthday)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
