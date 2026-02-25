3961dccb053553b0457ee52c3ca5fdaa9acf1894
meg44k <yugomegane@gmail.com>
2026-02-25 10:39:47 +0900

    Add: データベースの初期設定を実装しました

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shii-park/friends/internal/db"
+	"github.com/shii-park/friends/internal/sqlc"
 )
 
 func main() {
+	// DBのセットアップ
+	dbConn, err := db.Setup("postgre", "db", 2556, "user", "password", "dbname", "disable")
+	if err != nil {
+		fmt.Errorf("データベースのセットアップに失敗しました: %w", err)
+	}
+
+	queries := sqlc.New(dbConn)
 	r := gin.Default()
+
+	_ = queries // 未使用によるコンパイルエラー回避のために一時的に代入しています．クエリが必要になった時に，queriesからもらってください．その際にこの行は削除してください．
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
---
+++ internal/db/db.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"database/sql"
+	"strconv"
+)
+
+// ex.
+//
+// dbDriver = postgreSQL
+//
+//	dsn = "host=127.0.0.1 port=5432 user=user password=password dbname=dbname sslmode=disable"
+func Setup(dbDriver string, host string, port int, user string, password string, dbname string, sslmode string) (*sql.DB, error) {
+	dsn := "host=" + host + " port=" + strconv.Itoa(port) + " user=" + user + " dbname=" + dbname + " sslmode=" + sslmode
+	db, err := sql.Open(dbDriver, dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		// 接続に失敗した場合は、確保したリソースを解放してからエラーを返す
+		db.Close()
+		return nil, err
+	}
+	return db, err
+}
